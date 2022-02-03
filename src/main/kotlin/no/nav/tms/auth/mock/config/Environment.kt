36ae96b4514b5cdca6794bcf85b2ed2ab35274a6
Christopher Santana <christopher.santana@nav.no>
2022-02-03 15:19:47 +0100

    Hardkoder azure tenant-id etter som det ikke valideres dynamisk.

--- src/main/kotlin/no/nav/tms/auth/mock/config/Environment.kt
+++ src/main/kotlin/no/nav/tms/auth/mock/config/Environment.kt
@@ -5,6 +5,6 @@ import no.nav.personbruker.dittnav.common.util.config.StringEnvVar.getEnvVar
 data class Environment(
     val localUrl: String = getEnvVar("LOCAL_URL"),
     val tokendingsPrivateJwk: String = getEnvVar("TOKENDINGS_PRIVATE_JWK"),
-    val azureTenantId: String = getEnvVar("AZURE_TENANT_ID"),
-    val azurePrivateJwk: String = getEnvVar("AZURE_PRIVATE_JWK")
+    val azurePrivateJwk: String = getEnvVar("AZURE_PRIVATE_JWK"),
+    val azureTenantId: String = "tms-auth-mock"
 )
